49c1e3d6b80cc0d42565ba55b36b3618a49ec65d
cesaraugustopimenta <cesar.pimenta@alstratech.com>
2020-05-25 10:42:23 -0300

    resolvers - Repos, info, search - QueryType

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from ariadne import graphql_sync, make_executable_schema, load_schema_from_path, ObjectType, QueryType, MutationType
 from ariadne.constants import PLAYGROUND_HTML
-from flask import Flask, request, jsonify, escape
+from flask import Flask, request, jsonify
 import resolvers as r
 import os
 
@@ -8,32 +8,22 @@ import os
 type_defs = load_schema_from_path('schema.graphql')
 
 query = QueryType()
-studentType = ObjectType('Student')
-classType = ObjectType('Class')
-userType = ObjectType('User')
 
-mutation = MutationType()
-mutation.set_field('createStudent', r.resolve_student_create)
-mutation.set_field('createClass', r.resolve_class_create)
-mutation.set_field('addStudentToClass', r.resolve_add_student_to_class)
-mutation.set_field('createUser', r.resolve_user_create)
+query.set_field('githubSearchUser', r.resolve_apigithub_search_user)
+query.set_field('githubListReposName', r.resolve_apigithub_list_repos)
+query.set_field('githubReposInfo', r.resolve_apigithub_repos_info)
+query.set_field('githubReposInfoOptions', r.resolve_apigithub_repos_info_options)
 
+# mutation = MutationType()
+# mutation.set_field('createRepos', r.resolve_create_repos)
 
-query.set_field('studentById', r.resolve_student_by_id)
-query.set_field('classById', r.resolve_class_by_id)
-query.set_field('students', r.resolve_students)
-query.set_field('classes', r.resolve_classes)
-query.set_field('users', r.resolve_users)
-classType.set_field('students', r.resolve_students_in_classes)
-
-schema = make_executable_schema(type_defs, [studentType, classType, query, mutation])
+schema = make_executable_schema(type_defs, query)
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+    return "access /graphql"
 
 
 @app.route('/graphql', methods=['GET'])
--- resolvers.py
+++ resolvers.py
@@ -1,102 +1,43 @@
-import uuid
+import requests
+# import requests.auth 
 
-DB = {
-    "students": [],
-    "classes": [],
-    "users": [],
-}
+def resolve_apigithub_search_user(_, info, _username):
+    response = requests.get('https://api.github.com/users/{}' .format(_username))
+    try: 
+        resolve = response.json()['html_url']
+        return resolve
+    except Exception as e:
+        return "Usuario não encontrado"
+    
 
+def resolve_apigithub_list_repos(_, info, _username):
+    response = requests.get('https://api.github.com/users/{}/repos' .format(_username))
+    name_repos = []
+    try:
+        for repositorio in response.json():
+            if repositorio['private'] == False:
+                name_repos.append(repositorio['name'])
+        return name_repos
+    except Exception as e:
+        name_repos.append('Não encontrado')
+        return name_repos
 
-class User:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age 
-        self.id = uuid.uuid4().__str__()
+def resolve_apigithub_repos_info(_, info, _username, _repos):
+    response = requests.get('https://api.github.com/users/{}/repos' .format(_username))
+    try:
+        for repositorio in response.json():
+            list_options = repositorio.keys()
+            return list_options
+    except Exception as e:
+        return "repositório não encontrado"
 
-    def response(self):
-        temp = {
-            "name": self.name,
-            "age": self.age,
-            "id": self.id
-        }
-        return temp
-
-class Student:
-    def __init__(self, name):
-        self.name = name
-        self.id = uuid.uuid4().__str__()
-
-    def response(self):
-        temp = {
-            "name": self.name,
-            "id": self.id
-        }
-        return temp
-
-
-class Class:
-    def __init__(self, name):
-        self.name = name
-        self.id = uuid.uuid4().__str__()
-        self.students = []
-
-    def response(self):
-        return {
-            "name": self.name,
-            "id": self.id,
-            "students": self.students
-        }
-
-def resolve_student_create(_, info, name):
-    student = Student(name).response()
-    DB['students'].append(student)
-    return student
-
-
-def resolve_student_by_id(_, info, _id):
-    for student in DB['students']:
-        if student['id'] == _id:
-            return student
-
-
-def resolve_students(_, info):
-    return DB['students']
-
-
-def resolve_user_create(_, info, name, age):
-    user= User(name, age).response()
-    DB['users'].append(user)
-    return user
-
-def resolve_users(_, info):
-    return DB['users']
-
-
-def resolve_class_create(_, info, name):
-    class_object = Class(name).response()
-    DB['classes'].append(class_object)
-    return class_object
-
-
-def resolve_class_by_id(_, info, _id):
-    for class_object in DB['classes']:
-        if class_object['id'] == _id:
-            return class_object
-
-
-def resolve_classes(_, info):
-    return DB['classes']
-
-
-def resolve_add_student_to_class(_, info, class_id, student_id):
-    print(class_id + " " + student_id)
-    for index, class_object in enumerate(DB['classes']):
-        if class_object['id'] == class_id:
-            for student in DB['students']:
-                if student['id'] == student_id:
-                    DB['classes'][index]['students'].append(student)
-                    return DB['classes'][index]
-
-
-def resolve_students_in_classes(class_object, info):
-    return class_object['students']
+def resolve_apigithub_repos_info_options(_, info, _username, _repos, _options):
+    response = requests.get('https://api.github.com/users/{}/repos' .format(_username))
+    try:
+        for repositorio in response.json():
+            if repositorio['name'] == _repos:
+                repos_options = repositorio[_options]
+                return repos_options
+    except Exception as e:
+        print(e)
+        return "options não encontrada"
